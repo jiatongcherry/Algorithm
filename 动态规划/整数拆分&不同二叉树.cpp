fddd80ea6d6359a89ddccf2ae2ee4f256a4a97d8
jiatongcherry <jiatongluna@icloud.com>
2024-10-26 20:11:16 +0800

    Update and rename 整数拆分.cpp to 整数拆分&不同二叉树.cpp

---
+++ 动态规划/整数拆分&不同二叉树.cpp
@@ -0,0 +1,37 @@
+//ÕûÊý²ð·Ö
+//https://leetcode.cn/problems/integer-break/
+
+class Solution {
+public:
+    int integerBreak(int n) {
+        vector<int> dp(n + 1);
+        dp[2] = 1;
+        for (int i = 3; i <= n; i++)
+        {
+            for (int j = 1; j < i - 1; j++)
+            {
+                dp[i] = max(dp[i], max((i - j) * j, dp[i - j] * j));
+            }
+        }
+        return dp[n];
+    }
+};
+
+//不同的二叉搜索树
+//https://leetcode.cn/problems/unique-binary-search-trees/
+//注意理解dp[i]的含义，同时如何推导dp公式
+class Solution {
+public:
+    int numTrees(int n) {
+        vector<int> dp(n+1);
+        dp[0] = 1;
+        for(int i = 1; i <= n; i++)
+        {
+            for(int j = 1; j <= i; j++)
+            {
+                dp[i] += dp[j-1] * dp[i-j];
+            }
+        }
+        return dp[n];
+    }
+};
