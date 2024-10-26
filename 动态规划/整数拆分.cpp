89c572ab7ede085c3466cee83e45298ca021f010
jiatongcherry <jiatongluna@icloud.com>
2024-10-26 19:36:12 +0800

    Add files via upload

---
+++ 动态规划/整数拆分.cpp
@@ -0,0 +1,18 @@
+//ÕûÊý²ð·Ö
+//https://leetcode.cn/problems/integer-break/
+
+class Solution {
+public:
+    int integerBreak(int n) {
+        vector<int> dp(n + 1);
+        dp[2] = 1;
+        for (int i = 3; i <= n; i++)
+        {
+            for (int j = 1; j < i - 1; j++)
+            {
+                dp[i] = max(dp[i], max((i - j) * j, dp[i - j] * j));
+            }
+        }
+        return dp[n];
+    }
+};
